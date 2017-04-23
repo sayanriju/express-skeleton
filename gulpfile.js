35fb4ce0e5f2970f39c10e7a9e543301f71a1268
Sayan 'Riju' Chakrabarti <s26c.sayan@gmail.com>
2017-04-23 21:04:41 +0530

    ESLint Added

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,55 @@
 // Dependencies
-var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
-var notify = require('gulp-notify');
-var livereload = require('gulp-livereload');
+const gulp = require("gulp")
+const nodemon = require("gulp-nodemon")
+// const notify = require("gulp-notify")
+const livereload = require("gulp-livereload")
+const eslint = require("gulp-eslint")
 
-// Task
-gulp.task('server', function() {
-	// listen for changes
-	livereload.listen();
-	// configure nodemon
-	nodemon({
-		// the script to run the app
-//	  exec: 'node-inspector & node --debug',
-		nodeArgs: ['--debug'],
-		script: 'bin/www',
-		ext: 'js ejs json css html',
-		"ignore": ["public/*"],
-	}).on('restart', function(){
-		// when the app has restarted, run livereload.
-		gulp.src('*.*')
-			.pipe(livereload({ start: true }))
-			// .pipe(notify('Restarted Server & Reloading page...'));
-	})
-});
+// Tasks
+gulp.task("lint", () => {
+  // ESLint ignores files with "node_modules" paths.
+  // So, it's best to have gulp ignore the directory as well.
+  // Also, Be sure to return the stream from the task;
+  // Otherwise, the task may end before the stream has finished.
+  gulp.src(["**/*.js", "!node_modules/**"])
+    // eslint() attaches the lint output to the "eslint" property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
+    .pipe(eslint.failAfterError())
+})
 
-gulp.task('client', function() {
-  gulp.src('public/**/*.{js,css,html}')
-    .pipe(livereload({ start: true }));
-});
+gulp.task("server", () => {
+  // listen for changes
+  livereload.listen()
+  // configure nodemon
+  nodemon({
+  // the script to run the app
+  // exec: 'node-inspector & node --debug',
+  // nodeArgs: ['--debug'],
+    script: "bin/www",
+    ext: "js ejs json css html",
+    ignore: ["public/*"],
+  }).on("restart", () => {
+  // when the app has restarted, run livereload.
+    gulp.src("*.*")
+    .pipe(livereload({ start: true }))
+    // .pipe(notify('Restarted Server & Reloading page...'));
+  })
+})
 
-gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch('public/**/*.{js,css,html}', ['client']);
-});
+gulp.task("client", () => {
+  gulp.src("public/**/*.{js,css,html}")
+    .pipe(livereload({ start: true }))
+})
 
-gulp.task('default', ['watch', 'server']);
+gulp.task("watch", () => {
+  livereload.listen()
+  gulp.watch("public/**/*.{js,css,html}", ["client"])
+})
+
+gulp.task("default", ["watch", "server"])
