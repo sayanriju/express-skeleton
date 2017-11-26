4cc4779f9ad7bcefeb3f080424b305d3476d1617
Sayan "Riju" Chakrabarti <s26c.sayan@gmail.com>
2017-11-26 23:47:16 +0530

    Morgan logger disabled for testing environments

--- app.js
+++ app.js
@@ -41,7 +41,9 @@ app.set("view engine", "ejs")
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger("dev"))
+if (process.env.NODE_ENV && !process.env.NODE_ENV.startsWith("test")) {
+  app.use(logger("dev"))
+}
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
