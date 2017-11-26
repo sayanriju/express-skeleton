b12d0532fd341c68b66fad9cbd005ae78ab9d777
Sayan "Riju" Chakrabarti <s26c.sayan@gmail.com>
2017-11-26 23:40:03 +0530

    Log errors on server console in development

--- app.js
+++ app.js
@@ -63,6 +63,12 @@ app.use((req, res, next) => {
   next(err)
 })
 
+// log errors in development
+app.use(function (err, req, res, next) {
+  if (req.app.get("env") === "development") console.error(err.stack)
+  next(err)
+})
+
 // error handler
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
